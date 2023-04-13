aabc9579b0e858cf18fcab63ecb22622313aa196
MarcelRusu <marcelrusu@protonmail.com>
2023-04-12 22:27:00 -0400

    ToReactChildren lets you define objects that can be used in templates.

--- experiments/react/src/react.js
+++ experiments/react/src/react.js
@@ -3,16 +3,21 @@ import * as ReactDOM from "react-dom/client";
 import * as React from "react";
 export default ReactDOM;
 
+export class Atom {
+  constructor(state, setState) {
+    this.val = state;
+    this.setState = setState;
+  }
+  update(f) {
+    return this.setState(call.bind(f));
+  }
+  set(val) {
+    return this.setState(val);
+  }
+}
+
 export function use_state(initial) {
   let [state, setState] = React.useState(initial);
 
-  return {
-    val: state,
-    update(f) {
-      return setState(globalThis["call"].bind(f));
-    },
-    set(val) {
-      return setState(val);
-    },
-  };
+  return new Atom(state, setState);
 }
