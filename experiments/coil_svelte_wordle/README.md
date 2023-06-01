e9c5d072e557c9ba0ffcd3e6cdcae9904db199a8
MarcelRusu <marcelrusu@protonmail.com>
2023-06-01 17:06:02 -0400

    remove svelte experiment from repo - its out of date, and should be its own repo

--- experiments/coil_svelte_wordle/README.md
+++
@@ -1,3 +0,0 @@
-# Svelte/Coil Wordle Clone
-
-to make sure this works, copy the `dist` folder from root of `coil-lang` into this directory
--- experiments/coil_svelte_wordle/src/Row/EmptyRow.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="coil">
-  import Row from "./Row.svelte"
-  let matched = (0..5).fill(:empty)
-</script>
-
-<Row word="" {matched} />
