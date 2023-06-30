0c7298a4b6eb992139a2ed7e9b7ae112b3c4ab6d
MarcelRusu <marcelrusu@protonmail.com>
2023-06-30 15:07:40 -0400

    slightly better react shims.

--- experiments/react-playground/main.js
+++ experiments/react-playground/main.js
@@ -1,3 +1,3 @@
-import "@coil-lang/compiler/src/std/js_prelude";
-import "@coil-lang/compiler/src/std/prelude.coil";
+import "../../compiler/src/std/js_prelude";
+import "../../compiler/src/std/prelude.coil";
 import "./src/main.coil";
--- experiments/react-playground/vite.config.js
+++ experiments/react-playground/vite.config.js
@@ -3,4 +3,9 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [coil()],
+  esbuild: {
+    supported: {
+      "top-level-await": true, //browsers can handle top-level-await features
+    },
+  },
 });
