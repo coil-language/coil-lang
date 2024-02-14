308f0e3d16a987ef6307b4ceb96138c918905b48
MarcelRusu <marcelrusu@protonmail.com>
2024-02-14 15:23:51 -0500

    0.1.15 - add Collection:delete! - fix from_js typo

--- compiler/src/std/collection.mjs
+++ compiler/src/std/collection.mjs
@@ -2,11 +2,17 @@ import Meta from "./meta.mjs";
 import { ObjectLiteral } from "./globals.mjs";
 
 const Collection = Object.freeze({
-  at: Symbol("coil-lang/@0.1.0/std/collection/Collection:at"),
-  len: Symbol("coil-lang/@0.1.0/std/collection/Collection:len"),
-  "empty?": Symbol("coil-lang/@0.1.0/std/collection/Collection:empty?"),
-  "has?": Symbol("coil-lang/@0.1.0/std/collection/Collection:has?"),
+  at: Symbol("coil-lang/@0.1.15/std/collection/Collection:at"),
+  len: Symbol("coil-lang/@0.1.15/std/collection/Collection:len"),
+  "empty?": Symbol("coil-lang/@0.1.15/std/collection/Collection:empty?"),
+  "has?": Symbol("coil-lang/@0.1.15/std/collection/Collection:has?"),
+  "delete!": Symbol("coil-lang/@0.1.15/std/collection/Collection:delete!"),
 });
+
+ObjectLiteral.prototype[Collection["delete!"]] = function (key) {
+  delete this[key];
+  return this;
+};
 
 ObjectLiteral.prototype[Collection.at] = function (key) {
   return this[key];
@@ -38,6 +44,11 @@ Array.prototype[Collection["empty?"]] = function () {
 
 Array.prototype[Collection["has?"]] = function (value) {
   return this.some((elem) => elem[Meta["=="]](value));
+};
+
+Array.prototype[Collection["delete!"]] = function (idx) {
+  this.splice(idx, 1);
+  return this;
 };
 
 String.prototype[Collection.at] = function (idx) {
@@ -72,6 +83,11 @@ Map.prototype[Collection["has?"]] = function (key) {
   return this.has(key);
 };
 
+Map.prototype[Collection["delete!"]] = function (key) {
+  this.delete(key);
+  return this;
+};
+
 Set.prototype[Collection.at] = function (value) {
   if (this.has(value)) {
     return value;
@@ -88,6 +104,11 @@ Set.prototype[Collection["empty?"]] = function () {
 
 Set.prototype[Collection["has?"]] = function (value) {
   return this.has(value);
+};
+
+Set.prototype[Collection["delete!"]] = function (value) {
+  this.delete(value);
+  return this;
 };
 
 export default Collection;
